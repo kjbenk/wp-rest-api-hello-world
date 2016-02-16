8a34c38a89052c837cb9f1c2dacc1c4455293f1c
Kyle <kjbenk@gmail.com>
2016-02-16 10:39:57 -0500

    Added Unit Test

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -1,10 +1,13 @@
 <?php
 
-class SampleTest extends WP_UnitTestCase {
+class WPRAHW_Test extends WP_UnitTestCase {
 
-	function test_sample() {
+	function test_wprahw() {
+
+		$response = wp_remote_get( 'http://local.wordpress.dev/wp-json/wprahw/hello-world' );
+		$data = wp_remote_retrieve_body( $response );
+
 		// replace this with some actual testing code
-		$this->assertTrue( true );
+		$this->assertEquals( json_decode( $data ), 'Hello World' );
 	}
 }
-
